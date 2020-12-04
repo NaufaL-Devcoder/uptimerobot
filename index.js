28fefbb76166e0da3f2a4e0840ebd6986f12c2c0
NaufaL-Devcoder <75226643+NaufaL-Devcoder@users.noreply.github.com>
2020-12-04 06:19:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://atlantic-legendary-decimal.glitch.me"]
+const urls = ["https://flannel-equinox-cardigan.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
